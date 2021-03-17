ebdafdaf42252c0b0a12de9698b5828d17a0f59e
Francesco Calvi <calvi.fra@gmail.com>
2021-03-17 22:57:54 +0100

    feat: backend working

--- README.md
+++ README.md
@@ -1,3 +1,99 @@
 # Random Number Generator
 
 This is a simple fullstack random number generator program developer by Francesco (me) during the Lacerba node and typescript development course
+
+## Install
+
+ ```bash
+ npm install
+ ```
+
+ ## Usage backend
+
+ ```bash
+ ts-node main.ts
+ ```
+
+ example output:
+
+ ```
+  {
+  "Bari": [    
+    18,        
+    79,        
+    46,        
+    23,        
+    2
+  ],
+  "Cagliari": [
+    77,        
+    90,        
+    20,        
+    36,        
+    95
+  ],
+  "Firenze": [ 
+    42,        
+    3,
+    92,        
+    30,        
+    61
+  ],
+  "Genova": [  
+    52,        
+    69,        
+    12,        
+    50,        
+    79
+  ],
+  "Milano": [
+    24,
+    7,
+    40,
+    16,
+    84
+  ],
+  "Napoli": [
+    68,
+    45,
+    95,
+    40,
+    52
+  ],
+  "Palermo": [
+    32,
+    22,
+    90,
+    49,
+    20
+  ],
+  "Roma": [
+    84,
+    40,
+    65,
+    67,
+    59
+  ],
+  "Torino": [
+    47,
+    70,
+    49,
+    51,
+    85
+  ],
+  "Venezia": [
+    66,
+    56,
+    14,
+    37,
+    33
+  ],
+  "Nazionale": [
+    86,
+    12,
+    45,
+    36,
+    3
+  ]
+}
+ ```
--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-console.log('Buongiorno!');
+function RNG(min: number, max: number) {
+  const rng = Math.random();
+  return Math.trunc(rng * (max-min) + min);
+  // rng * (max-min) + min
+  // se rng = 0 => min
+  // se rng = 1 => max
+  // dato che Math.random() genera un numero compreso tra 0 e 1 il risultato sarà sempre min  
+}
+
+/*creare una funzione che genera e mette numeri in un array */
+function RNGSequence(len: number, min: number, max: number) {
+if (len > max - min) {
+    throw new Error('cannot find ' + len + ' numbers beetwen ' + min + ' and ' + max);
+};
+// creare un ciclo per cui per ogni numero generato si controlla se è presente nell'array
+const res: number[] = [];
+while (res.length < len) {   
+  const rn = RNG(min, max);
+  if (res.includes(rn)){
+    continue; //** continue: salta un valore e prosegue */
+  } else {
+    res.push(rn);
+  }
+}
+return res;
+};
+
+// console.log('random list: ', myArr); 
+
+// lanciare la funzione RNGSequence per ogni città
+const ruote: string[] = ['Bari', 'Cagliari', 'Firenze', 'Genova', 'Milano', 'Napoli', 'Palermo', 'Roma',  'Torino', 'Venezia', 'Nazionale'];
+const estrazioni: {[ruota: string]: number [] } = {};
+
+ruote.map((el)=> {  
+  const estrazione = RNGSequence(5, 1, 100);
+  estrazioni[el] = estrazione;
+});
+
+console.log(JSON.stringify(estrazioni, null, 2));
